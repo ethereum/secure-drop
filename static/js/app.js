89929b134168489fddfba9e2ca0e3622e534a265
Oleh Aldekein <i.am@aldeke.in>
2023-02-24 19:33:04 +0200

    This adds multi-file upload v2, needs testing

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,77 @@
+Dropzone.options.dropzoneArea = {
+	maxFilesize: 5, // Max file size per file
+	maxFiles: 10, // Max number of files
+	url: '/fake',
+	paramName: 'attachment',
+	autoProcessQueue: false,
+	autoQueue: false,
+	addRemoveLinks: true,
+	uploadMultiple: true,
+	dictDefaultMessage: 'You may drop files here to upload',
+	init: function() {
+		this.on("addedfile", function(file) {
+		  // Calculate the total added file size
+		  var totalSize = this.files.reduce(function(total, f) {
+			return total + f.size;
+		  }, 0);
+		  
+		  // If the total added file size is greater than 15 MB, remove the file
+		  if (totalSize > 15 * 1024 * 1024) {
+			this.removeFile(file);
+			alert("Total file size exceeded the limit of 15 MB, file cannot be added.");
+		  }
+		});
+		
+		this.on("removedfile", function(file) {
+		  // Calculate the total added file size
+		  var totalSize = this.files.reduce(function(total, f) {
+			return total + f.size;
+		  }, 0);
+		  
+		  // Log the total added file size
+		  console.log("Total file size: " + totalSize);
+		});
+	}
+};
+
+function initDropzone() {
+	var myDropzone = new Dropzone(document.getElementById("dropzone-area"), {
+		url: '/fake',
+		paramName: 'attachment',
+		autoProcessQueue: false,
+		autoQueue: false,
+		addRemoveLinks: true,
+		uploadMultiple: true,
+		dictDefaultMessage: 'You may drop files here to upload',
+		maxFilesize: 15, // Max file size per file
+		maxFiles: 10, // Max number of files
+		init: function() {
+		  this.on("addedfile", function(file) {
+			// Calculate the total added file size
+			var totalSize = this.files.reduce(function(total, f) {
+			  return total + f.size;
+			}, 0);
+			
+			// If the total added file size is greater than 15 MB, remove the file
+			if (totalSize > 15 * 1024 * 1024) {
+			  this.removeFile(file);
+			  alert("Total file size exceeded the limit of 15 MB.");
+			}
+		  });
+		  
+		  this.on("removedfile", function(file) {
+			// Calculate the total added file size
+			var totalSize = this.files.reduce(function(total, f) {
+			  return total + f.size;
+			}, 0);
+			
+			// Log the total added file size
+			console.log("Total file size: " + totalSize);
+		  });
+		}
+	  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	var text = document.getElementById("text");
 	var recipient = document.getElementById("recipientSelect");
@@ -20,20 +94,25 @@ document.addEventListener('DOMContentLoaded', function() {
 		messageLabel.innerHTML = (recipient.value == "esp") ? "Please include Grant ID in the message. Example: \"FY22-0123\":" : "Message:";
 	});
 
-	// Multi file upload
-	const fileSelector = document.getElementById('file-selector');
-	fileSelector.addEventListener('change', (event) => {
-		const fileList = event.target.files;
-        const fileCount = form.elements['file-count'].value;
+	const addFileButton = document.getElementById('add-file-button');
+	addFileButton.addEventListener('click', (event) => {
+		// Get a reference to the file input element used by Dropzone.js
+		var fileInput = document.querySelector(".dz-hidden-input");
 
-        if (fileList.length > fileCount) {
-            alert("Action cancelled! Only " + fileCount + " files can be uploaded in one submission. Make several submissions if you need to upload more files.");
-			event.target.value = "";
-            return false;
-        }
+		// Simulate a click event on the file input element
+		fileInput.click();
+	});
 
-		for (var i=0; i < fileList.length; i++) {
-			current_file = fileList[i];
+	// Multi file upload meets encryption
+	document.forms[0].addEventListener("submit", function(evt) {
+		evt.preventDefault();
+		const form = evt.target;
+		
+		const selectedFiles = Dropzone.instances[0].files || [];
+		var promises = [];
+
+		for (var i=0; i < selectedFiles.length; i++) {
+			current_file = selectedFiles[i];
 			filename_field = form.elements['filename-' + i];
 			attachment_field = form.elements['attachment-' + i];
 
@@ -45,23 +124,28 @@ document.addEventListener('DOMContentLoaded', function() {
 			reader.addEventListener('load', (event) => {
 				var arrayBuffer = event.target.result;
 				var fileData = new Uint8Array(arrayBuffer);
-				encryptFile(fileData).then(function(encrypted_file) {
+				var encryptionPromise = encryptFile(fileData).then(function(encrypted_file) {
 					// parameters for `addEventListener` in a *for loop* need a special treatment
 					// the only working solution seems to be: https://stackoverflow.com/a/11986895
 					event.target.attachment_field.value = encrypted_file;
 				});
+				promises.push(encryptionPromise);
 			});
 			reader.readAsArrayBuffer(current_file);
 		}
-	}, false);
 
-	document.forms[0].addEventListener("submit", function(evt) {
-		evt.preventDefault();
-		encrypt(text.value).then(function(encrypted_msg) {
-			form.elements['message'].value = encrypted_msg;
-			form.elements['recipient'].value = recipient.value;
-			form.submit();
+		// Wait for all encryption promises to resolve before submitting the form
+		Promise.all(promises).then(() => {
+			// Defer the Promise.all() call to the next event loop iteration to ensure that the DOM is fully updated
+			Promise.resolve().then(() => {
+				encrypt(text.value).then(function(encrypted_msg) {
+					form.elements['message'].value = encrypted_msg;
+					form.elements['recipient'].value = recipient.value;
+					form.submit();
+				});
+			});
 		});
+
 		return true;
 	});
 });
@@ -85,7 +169,7 @@ async function encryptFile(file) {
 	var publicKeyArmored = publicKeys[recipientId];
 	const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
 	const message = await openpgp.createMessage({ binary: file });
-	const encrypted = await openpgp.encrypt({
+	const encrypted = openpgp.encrypt({
 		message,
 		encryptionKeys: publicKey,
 		format: 'armored'
